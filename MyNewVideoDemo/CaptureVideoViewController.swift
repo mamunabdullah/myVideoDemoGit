bbf15767f47b2dd82d149105a7070ea0e02f26a9
mamunabdullah <mamunsarkar18@gmail.com>
2020-12-24 18:32:46 +0600

    first commit

---
+++ MyNewVideoDemo/CaptureVideoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CaptureVideoViewController.swift
+//  MyNewVideoDemo
+//
+//  Created by Abdullah Al Mamun on 24/12/20.
+//
+
+import UIKit
+
+class CaptureVideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
